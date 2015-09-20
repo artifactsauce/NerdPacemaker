bd7f4facf3b8e190ed9711e2cc8aeafd810ee685
Kenji Akiyama <artifactsauce@gmail.com>
2015-09-20 17:06:03 +0900

    move the script section to a source file.

---
+++ app/elements/timer/lib/app.js
@@ -0,0 +1,13 @@
+(function() {
+  Polymer({
+    is: 'timer-main',
+    properties: {
+      time: {
+        type: String,
+        value: '25:00',
+        notify: true
+      }
+    }
+  });
+
+})();
