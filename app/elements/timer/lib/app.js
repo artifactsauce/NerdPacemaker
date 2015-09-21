2d28c4dafe59fd853357d3f2ce0efc681c739e47
Kenji Akiyama <artifactsauce@gmail.com>
2015-09-22 02:46:02 +0900

    enable to start and stop timer.

--- app/elements/timer/lib/app.js
+++ app/elements/timer/lib/app.js
@@ -1,5 +1,18 @@
 (function() {
   'use strict';
+  var converter = {
+    s2m: function(s) {
+      var minutes = Math.floor(s / 60);
+      var seconds = s % 60;
+      return minutes.toString() + ':' + ('0' + seconds.toString()).slice(-2);
+    },
+    m2s: function(m) {
+      var arrayString = m.split(':');
+      var minutes = Number(arrayString[0]);
+      var seconds = Number(arrayString[1]);
+      return minutes * 60 + seconds;
+    }
+  };
   Polymer({
     is: 'timer-main',
     properties: {
@@ -14,9 +27,23 @@
         notify: true
       }
     },
-    start: function() { console.log('start'); },
-    stop:  function() { console.log('stop'); },
+    start: function() {
+      var self = this;
+      this.intervalId = setInterval(
+        function(){self.time_elapses()},
+        1000
+      );
+      console.debug('start - ID: ' + this.intervalId);
+    },
+    stop:  function() {
+      clearInterval(this.intervalId);
+      console.debug('stop -  ID: ' + this.intervalId);
+    },
     next:  function() { console.log('next'); },
+    time_elapses: function() {
+      var now = converter.m2s(this.time);
+      this.time = converter.s2m(now - 1);
+    },
     ready: function() {
       console.log(this);
     }
